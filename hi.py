ab82bce332b339a62925ae822de22b0eed020c12
Martin Wang <ms0695961@gmail.com>
2021-06-03 14:23:16 +0800

    Add new file

---
+++ hi.py
@@ -0,0 +1,7 @@
+name = input ('Please enter your name: ')
+print('Hey', name)
+ 
+height = input('Please enter your height: ')
+print('Your height is', height, 'cm')
+weight = input('Please enter your weight: ')
+print('Your weight is ', weight, 'kg')
